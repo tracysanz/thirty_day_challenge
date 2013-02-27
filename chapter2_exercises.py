fc9d4d50ffb2b2ff57ae0de3ca69aa70bcdf9ef8
tracysanz <tracysanz@yahoo.com>
2013-02-27 18:27:33 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,10 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Tamar Schanfeld
+
+# 2.1 - octal (base-eight)
+# 2.2 - entering 5, x = 5, x + 1 displays 5, (nothing displayed), and 6 in the interpreter
+# 2.2 - saving it as a file and running it will not generate anything on screen
+# 2.2 - adding print statements to the first and third line, saving the file, and running it will show 5 and 6 on the screen
+# 2.3 - 1) 8 2) 8.5 3) 4.0 4) 11 5) '.....'
+# 2.4 - 1) 523.5987755983333 2) 945.45 3) 7:30:6
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,46 @@
 # Exercises for chapter 3: Problems 3.1, 3.2, 3.3, and 3.4 in Think Python
 
-# Name
+# Tamar Schanfeld
+# 3.1 Traceback (most recent call last):
+#  File "C:/Users/Tamar/Documents/program/thirty_day_challenge/test2", line 1, in <module>
+#    repeat_lyrics()
+# NameError: name 'repeat_lyrics' is not defined
+# 3.2 NameError: global name 'print_lyrics' is not defined
+# 3.3
+def right_justify(word):
+    print (' '*(70-len(word)))+word
+# 3.4  1) spam appears twice 
+# 3.4 2)
+def do_twice(f, word):
+    f(word)
+    f(word)
+
+# 3.4 3) 
+def print_twice(word):
+    print word
+    print word
+    
+# 3.4 4)
+ def do_twice(f, word):
+    f(word)
+    f(word)
+
+def print_twice(word):
+    print word
+
+do_twice(print_twice,'spam')
+
+# 3.4 5)
+def do_twice(f, word):
+    f(word)
+    f(word)
+
+def print_twice(word):
+    print word
+    print word
+
+def do_four(f, word):
+    do_twice(f, word)
+    do_twice(f, word)
+
+do_four(print_twice, 'spam')
